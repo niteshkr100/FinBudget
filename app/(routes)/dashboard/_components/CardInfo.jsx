ba4b0b2222d05a07b2815a87fb9dd5df9210359d
Nitesh Kumar <niteshkumar@Niteshs-MacBook-Air.local>
2024-08-24 19:37:00 +0530

    new update

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { PiggyBank, Receipt, Wallet } from "lucide-react"
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
  
 const CardInfo = ({budgetList}) => {
--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 import Siderbar from "./_components/Siderbar";
 import DashboardHeader from "./_components/DashboardHeader";
